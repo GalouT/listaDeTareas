60c516ca2c5af2608db1d849c7525b525c4feac0
Galo <admin@admins-MacBook-Pro.local>
2022-08-21 21:04:36 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,52 @@
+const fecha = document.getElementById("fecha")
+const input = document.querySelector("input")
+const botonAgregar = document.querySelector("#botonAgregar")
+const ul = document.querySelector("ul")
+const sinTareas = document.querySelector(".sinTareas")
+
+const FECHA = new Date()
+fecha.innerHTML = FECHA.toLocaleDateString('es',{weekday:'long',month:'long',day:'numeric',year:'numeric'})
+
+botonAgregar.addEventListener("click", (e)=>{
+    e.preventDefault()
+
+    if(input.value === ""){
+        return
+    }
+    
+    const texto = input.value
+
+    const li = document.createElement("li")
+    const p = document.createElement("p")
+    p.textContent = texto
+
+    li.appendChild(p)
+    li.appendChild(addDeleteBtn())
+    ul.appendChild(li)
+    
+
+    input.value = ""
+
+    sinTareas.style.display = "none"
+})
+
+
+function addDeleteBtn(){
+    const botonBorrar = document.createElement("button")
+
+    botonBorrar.textContent = "-"
+
+
+    botonBorrar.addEventListener("click", (e)=>{
+        const item = e.target.parentElement
+        ul.removeChild(item)
+
+
+        const items = document.querySelectorAll('li')
+        if(items.length === 0){
+            sinTareas.style.display = "block"
+        }
+    })
+
+    return botonBorrar
+}
